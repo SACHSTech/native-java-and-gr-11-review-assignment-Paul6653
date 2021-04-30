7e78956fd6bf1d13f71e06e3a944e6a81761c3fd
Paul Tran <>
2021-04-30 19:24:40 +0000

    review 1 test 1

--- src/gr11review/part1/Review1.java
+++ src/gr11review/part1/Review1.java
@@ -1,1 +1,16 @@
 package gr11review.part1;
+
+import java.io.*;
+
+public class Review1{
+  public static void main(String[] args) throws IOException{
+
+    Buffered key = new BufferedReader(new InputStreamReader(system.in));
+
+    System.out.println("Enter the month number: ");
+    int month = Integer.parseInt(key.readLine());
+
+    System.out.println("Enter the day number: ");
+    int day = Integer.parseInt(key.readLine());
+  }
+}
