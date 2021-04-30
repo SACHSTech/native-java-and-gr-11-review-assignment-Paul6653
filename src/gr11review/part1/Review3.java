f8e004d45e162d49f6339077111028abc616717b
Paul Tran <>
2021-04-30 20:21:53 +0000

    review 3 finished

--- src/gr11review/part1/Review3.java
+++ src/gr11review/part1/Review3.java
@@ -6,7 +6,15 @@ public class Review3{
   public static void main(String[] args) throws IOException{
 
     //prints odd numbers from 20 to 100
-    for (int i = 21; i < 100; i = i + 2){
+    for (int i = 21; i <= 100; i = i + 2){
+      System.out.println(i);
+    }
+
+    //blank line
+    System.out.println("");
+
+    //prints numbers from 29 to 2 in descending order
+    for (int i = 29; i >= 2; i--){
       System.out.println(i);
     }
   }
