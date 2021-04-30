a7e2207dad09f398ec61eeacdf06c06eeaa08d89
Paul Tran <>
2021-04-30 19:46:38 +0000

    review 1 finished

--- src/gr11review/part1/Review1.java
+++ src/gr11review/part1/Review1.java
@@ -5,12 +5,49 @@ import java.io.*;
 public class Review1{
   public static void main(String[] args) throws IOException{
 
-    Buffered key = new BufferedReader(new InputStreamReader(system.in));
+    BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
 
-    System.out.println("Enter the month number: ");
+    //Input month number
+    System.out.print("Enter the month number: ");
     int month = Integer.parseInt(key.readLine());
 
-    System.out.println("Enter the day number: ");
+    //Input day of month
+    System.out.print("Enter the day number: ");
     int day = Integer.parseInt(key.readLine());
+
+    int dayNumber = 0;
+
+    //convert month into #of days into the year
+    if (month == 1){
+      dayNumber = 0;
+    } else if (month == 2){
+      dayNumber = 31;
+    } else if (month == 3){
+      dayNumber = 59;
+    } else if (month == 4){
+      dayNumber = 90;
+    } else if (month == 5){
+      dayNumber = 120;
+    } else if (month == 6){
+      dayNumber = 151;
+    } else if (month == 7){
+      dayNumber = 181;
+    } else if (month == 8){
+      dayNumber = 212;
+    } else if (month == 9){
+      dayNumber = 243;
+    } else if (month == 10){
+      dayNumber = 273;
+    } else if (month == 11){
+      dayNumber = 304;
+    } else if (month == 12){
+      dayNumber = 334;
+    } 
+
+    //add day to dayNumber
+    dayNumber = dayNumber + day;
+
+    //print day of the year
+    System.out.println(dayNumber);
   }
 }
