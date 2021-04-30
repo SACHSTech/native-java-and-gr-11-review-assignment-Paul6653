c2928e9d2233eb8a785f716a5af1b3319c523647
Paul Tran <>
2021-04-30 20:17:14 +0000

    review 3 test 1

--- src/gr11review/part1/Review3.java
+++ src/gr11review/part1/Review3.java
@@ -1,1 +1,13 @@
 package gr11review.part1;
+
+import java.io.*;
+
+public class Review3{
+  public static void main(String[] args) throws IOException{
+
+    //prints odd numbers from 20 to 100
+    for (int i = 21; i < 100; i = i + 2){
+      System.out.println(i);
+    }
+  }
+}
